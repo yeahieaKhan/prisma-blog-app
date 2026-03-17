fafb5d41739f61e9f78f288f8c6f6d01b7118cd8
yeahieakhan <yeahieakhan2017@gmail.com>
2026-03-17 15:35:28 +0600

    updated form o

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import express, { Request, Response } from "express";
 import { postRouter } from "./modules/post/post.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app = express();
 
 // middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:8080 ",
+    credentials: true,
+  }),
+);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,18 @@
 import { betterAuth, string } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-
+import nodemailer from "nodemailer";
 // If your Prisma file is located elsewhere, you can change the path
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASS,
+  },
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -28,5 +38,22 @@ export const auth = betterAuth({
   },
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log({ user, url, token });
+      const info = await transporter.sendMail({
+        from: '"prisma blog app" <prismablog@ph.com>',
+        to: "yeahieakhan2018@gmail.com",
+        subject: "Hello ✔",
+        text: "Hello world?", // Plain-text version of the message
+        html: "<b>Hello world?</b>", // HTML version of the message
+      });
+
+      console.log("Message sent:", info.messageId);
+    },
   },
 });
